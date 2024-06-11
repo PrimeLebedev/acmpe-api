f0467227cd84efa5eb42187cac45a73d874d5d99
PrimeLebedev <126701364+PrimeLebedev@users.noreply.github.com>
2024-06-11 05:34:51 +0300

    add

--- calendar-service/api.py
+++ calendar-service/api.py
@@ -2,6 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
+API_ROOT = '/api/v1'
+CALENDAR_API_ROOT = API_ROOT + '/calendar'
 
-
-#@app.route('')
+@app.route(CALENDAR_API_ROOT + '/', methods=['GET'])
+def read():
+    return 'read'
